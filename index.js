5abd37a5c4073026f187ef94ba545d72aa0b8510
iamsumanshekhar <jena.sumansekhar2000@gmail.com>
2024-11-18 18:08:03 +0530

    update

--- index.js
+++ index.js
@@ -14,3 +14,34 @@ for(let i = 0; i < numbers.length; i++){
 
 console.log("Even Numbers : ", evenNumbers);
 console.log("Odd Numbers : ", oddNumbers);
+
+//prime number
+function isPrime(num) {
+    if(num < 2) return false;
+    for(let i = 2; i <= Math.sqrt(num); i++){
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+//array of prime numbers
+const primeArray = [];
+for(let i = 0; i < numbers.length; i++){
+    if(isPrime(numbers[i])){
+        primeArray.push(numbers[i]);
+    }
+}
+//To find max and min in array
+let max = primeArray[0];
+let min = primeArray[0];
+let sum = 0;
+
+for (let i = 0; i < primeArray.length; i++){
+    if(primeArray[i] > max) max = primeArray[i];
+    if(primeArray[i] < min) min = primeArray[i];
+    sum += primeArray[i];
+}
+
+console.log("Prime Numbers : ", primeArray);
+console.log("Minimum :", min);
+console.log("Maximun :", min);
+console.log("Sum :", sum);
